3d0435883642aa37e6cef5ef015bd311eacaf1dd
Erica Cannedy <encannedy@gmail.com>
2020-08-02 20:53:51 -0400

    react app  started

---
+++ my-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Portfolio from "./pages/portfolio/Portfolio";
+import "./container.css"
+
+
+class PortfolioContainer extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "About") {
+      return <About />;
+    } else if (this.state.currentPage === "Portfolio") {
+      return <Portfolio />;
+    } 
+  };
+
+ 
+
+  render() {
+    return (
+      <div className="navContainer">
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default PortfolioContainer;
